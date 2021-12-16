41a35ecca70d8bb31e90cdf94b416cdfc6e83a18
anrysliusar <assliusarenko@gmail.com>
2021-12-16 14:07:57 +0200

    add lab6

--- hello.c
+++ hello.c
@@ -4,9 +4,10 @@
 #include <linux/moduleparam.h>
 #include <linux/ktime.h>
 #include <linux/slab.h>
+#include <asm-generic/bug.h>
 
-MODULE_AUTHOR("Andrii Sliusarenko <assliusarenko@gmail.com>");
-MODULE_DESCRIPTION("Lab 5 AK");
+MODULE_AUTHOR("Andrii Sliusarenko<assliusarnko@gmail.com>");
+MODULE_DESCRIPTION("Lab 6");
 MODULE_LICENSE("Dual BSD/GPL");
 
 static uint numtimes = 1;
@@ -15,50 +16,51 @@ module_param(numtimes, uint, 0444);
 MODULE_PARM_DESC(numtimes, "Number of prints");
 
 struct my_struct {
-    struct list_head tlist;
-    ktime_t time;
+	struct list_head tlist;
+	ktime_t time;
 };
 
 static LIST_HEAD(my_list_head);
 
 static int __init hello_init(void)
-{
+{	
+	if (numtimes == 0) {
+		pr_warn("The parameter is 0");
+	} else if (numtimes >= 5 && numtimes <= 10) {
+		pr_warn("The parameter is in [5; 10]");
+	}
 
-    if (numtimes == 0) {
-		printk(KERN_WARNING "The parameter is 0\n");
-    } else if (numtimes >= 5 && numtimes <= 10) {
-		printk(KERN_WARNING "The parameter is between 5 and 10\n");
-    } else if (numtimes > 10) {
-		printk(KERN_WARNING "The parameter is > 10)\n");
-		return -EINVAL;
-    }
+	BUG_ON((numtimes > 10));
 
-    int i;
-		for (i = 0; i < numtimes; i++) {
+	int i;
+	for (i = 0; i <= numtimes; i++) {
 
-			struct my_struct *ms = NULL;
+		struct my_struct *ms = NULL;
 
+		if (i != numtimes)
+		{
 			ms = kmalloc(sizeof(struct my_struct), GFP_KERNEL);
-			ms->time = ktime_get();
-			list_add_tail(&ms->tlist, &my_list_head);
+		}
+		ms->time = ktime_get();
+		list_add_tail(&ms->tlist, &my_list_head);
 
-			printk(KERN_INFO "Hello, world!\n");
-    }
+		pr_info("Hello, world!\n");
+	}
 
-    return 0;
+	return 0;
 }
 
 static void __exit hello_exit(void)
 {
-    int count = 0;
-    struct my_struct *t, *tmp;
+	int count = 0;
+	struct my_struct *t, *tmp;
 
-    list_for_each_entry_safe(t, tmp, &my_list_head, tlist) {
+	list_for_each_entry_safe(t, tmp, &my_list_head, tlist) {
 		pr_info("i: %d; ktime = %lld\n", count++, ktime_to_ns(t->time));
 		list_del(&t->tlist);
 		kfree(t);
-    }
-    printk(KERN_INFO "Module unloaded\n");
+	}
+	pr_info("Module unloaded\n");
 }
 
 module_init(hello_init);
